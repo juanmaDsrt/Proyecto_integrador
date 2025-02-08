9ff7d38dce5ccff467f22ae41badadb9bde0abb5
Juan Manuel Florez <jmflorezrobledo@gmail.com>
2025-02-07 22:51:31 -0500

    nuevo archivo about.html

--- moviereviews/movie/views.py
+++ moviereviews/movie/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     "return HttpResponse('<h1>Welcome to Home Page</h1>')"
-    return render(request,'home.html',{'name': 'Greg Lim'})
+    return render(request,'home.html',{'name': 'Juan Manuel Florez'})
 
